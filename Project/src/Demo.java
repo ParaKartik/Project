e677c6e6783bc0376a085b37bfa578c950c1a99d
ParaKartik <21akulwar@gmail.com>
2023-02-03 18:01:54 +0530

    First Commit

---
+++ Project/src/Demo.java
@@ -0,0 +1,27 @@
+
+public class Demo {
+
+	public static void main(String[] args) {
+		
+		
+		 String ss= "kartik";
+	        String ab = "";
+	        
+	        for(int i = 0; i<ss.length();i++) {
+	        	  
+	        	     ab= ab+(char)(ss.charAt(i)-32);
+	        }
+	           System.out.println(ab);
+
+	      ///Lower
+		  String kk= "KARTIK";
+	      String aa = "";
+	      
+	      for(int i = 0; i<kk.length();i++) {
+	      	  
+	      	     aa= aa+(char)(kk.charAt(i)+32);
+	      }
+	         System.out.println(aa);
+	}
+
+}
